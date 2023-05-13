9666d91d8fc3f8d39b63d4e472084fcc362f0f7c
Caleb Lockwood <caleb.lockwood626@gmail.com>
2023-05-13 02:14:02 -0600

    comment model update

--- models/comment.js
+++ models/comment.js
@@ -3,8 +3,52 @@ const sequelize = require('../config/connection')
 
 class Comment extends Model {}
 
+Comment.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
 
+        user_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            references:  {
+                model: 'user',
+                key: 'id',
+            }
+        },
 
+        post_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            references: {
+                model: 'post',
+                key: 'id',
+            }
+        },
+
+        timestamp: {
+            type: DataTypes.DATE,
+            allowNull: false,
+        },
+
+        comment_text: {
+            type: DataTypes.TEXT,
+            allowNull: false,
+        } 
+    },
+
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: false,
+        modelName: 'comment'
+    },
+);
 
 
 
