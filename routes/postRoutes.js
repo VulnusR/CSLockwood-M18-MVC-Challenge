0a121b5741e505d9187541b7e9d8972f9227afef
Caleb Lockwood <caleb.lockwood626@gmail.com>
2023-05-13 03:25:58 -0600

    post Routes updated

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -30,6 +30,81 @@ router.get('/', async (req, res) => {
 });
 
 
+// GET post via id
+router.get('/:id', async (req, res) => {
+    try {
+        const singlePost = await Post.findByPk(req.params.id, {
+            attributes: ['id', 'title', 'user_id', 'timestamp', 'content'],
+            include: [
+                {
+                    model: User,
+                    attributes: ['username']
+                }, 
+                {
+                    model: Comment, 
+                    attributes: ['timestamp', 'comment_text']
+                }
+            ]
+        });
+
+        console.log(singlePost)
+
+        if(!singlePost) {
+            res.status(404).json({ message: "No posts with this ID!"});
+            return;
+        }
+        res.status(200).json(singlePost); 
+    } 
+    
+    catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+// Create post
+router.post('/', async (req, res) => {
+    try {
+        const newPost = await Post.create({
+            ...req.body,
+            user_id: req.session.user_id,
+            timestamp: Date.now()
+        })
+
+        if(!newPost) {
+            res.status(404).json({ message: 'Post info is required'});
+            return;
+        }
+
+        res.status(200).json(newPost);
+    } 
+    
+    catch (err) {
+        res.status(500).json(err);
+    }
+});   
+
+// update a post by id
+router.put('/:id', async (req, res) => {
+    try {
+        const postToUpdate = await Post.update(req.body, {
+            where: {
+                id: req.params.id,
+            },
+        });
+
+        if(!postToUpdate[0]) {
+            res.status(400).json({ message: 'No posts with this ID!' });
+            return;
+        }
+
+        res.status(200).json(postToUpdate); 
+    } 
+    catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+
 
 
 
