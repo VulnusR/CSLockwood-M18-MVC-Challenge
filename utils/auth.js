a5d56e68e9d7410a2ac469c0c5783c1a18961735
Caleb Lockwood <caleb.lockwood626@gmail.com>
2023-05-13 03:59:04 -0600

    utils added

---
+++ utils/auth.js
@@ -0,0 +1,12 @@
+const withAuth = (req, res, next) => {
+    // checks if logged in, otherwise redirects them to login page
+    if (!req.session.logged_in) {
+      res.redirect('/login');
+    } 
+    
+    else {
+      next();
+    }
+};
+  
+module.exports = withAuth;
