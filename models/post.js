5e06ffb85e677ba15576d6d06602763a24624f63
Caleb Lockwood <caleb.lockwood626@gmail.com>
2023-05-13 02:09:29 -0600

    post model updated/being reworked

--- models/post.js
+++ models/post.js
@@ -4,4 +4,48 @@ const sequelize = require('../config/connection')
 class Post extends Model {}
 
 
+Post.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    
+    title: {
+      type: DataTypes.STRING, 
+      allowNull: true,
+    },
+
+    user_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'user',
+        key: 'id'
+      },
+    },
+
+      timestamp: {
+        type: DataTypes.DATE, 
+        allowNull: false,
+      }, 
+
+      content: {
+        type: DataTypes.TEXT,
+        allowNull: false, 
+    }
+  },
+
+  {
+    sequelize,
+    timestamps: false,
+    reezeTableName: true,
+    underscored: false,
+    modelName: 'post'
+  }
+)
+
+
 module.exports = Post;
