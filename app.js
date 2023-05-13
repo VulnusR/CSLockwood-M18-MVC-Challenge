d39b0de7c1d5701728e5f779d5aa276b6d3b9d42
Caleb Lockwood <caleb.lockwood626@gmail.com>
2023-05-12 23:22:08 -0600

    files added, file structure updated, handlebars started--ripped some code for main from an older project

--- app.js
+++ app.js
@@ -1,9 +1,30 @@
 const express = require('express');
+const exphbs = require('express-handlebars');
+const path = require('path');
 const app = express();
 
-//middleware and configuration setup
+// Middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
 
-//routes
+// View engine setup
+app.engine('hbs', exphbs.engine({
+    extname: "hbs",
+    defaultLayout: 'main',
+    layoutsDir: path.join(__dirname, 'views/layouts'),
+    partialsDir: "", //path.join(__dirname, 'views/partials'),
+    // helpers: {
+     
+}));
+
+
+
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'views'));
+
+
+// Routes setup
 const signupRoutes = require('./routes/signup');
 const indexRoute = require('./routes/index');
 app.use(signupRoutes);
@@ -11,5 +32,5 @@ app.use(indexRoute);
 
 // Start the server
 app.listen(3000, () => {
-    console.log('Server is running on port 3000');
-  });
+  console.log('Server is running on port 3000');
+});
