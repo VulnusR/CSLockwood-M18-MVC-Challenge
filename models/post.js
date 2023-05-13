505364faf51f3f8edbba1898ae87345fb0abd940
Caleb Lockwood <caleb.lockwood626@gmail.com>
2023-05-13 02:06:02 -0600

    post model updated/being reworked

--- models/post.js
+++ models/post.js
@@ -1,25 +1,7 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
-module.exports = (sequelize, DataTypes) => {
-  class post extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
-    }
-  }
-  post.init({
-    title: DataTypes.STRING,
-    content: DataTypes.TEXT,
-    userId: DataTypes.INTEGER
-  }, {
-    sequelize,
-    modelName: 'post',
-  });
-  return post;
-};
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection')
+
+class Post extends Model {}
+
+
+module.exports = Post;
